39473a52abbbaedd6a6c02a10a10e311fe2ea989
Ved koshta <vkostha@trueigtech.com>
2026-04-27 17:59:53 +0530

    fixed landscape layout ui

--- src/components/LayoutLandscape.svelte
+++ src/components/LayoutLandscape.svelte
@@ -2,17 +2,19 @@
 	import { stateUi } from 'state-shared';
 	import { BLACK } from 'constants-shared/colors';
 	import { MainContainer } from 'components-layout';
-	import { Container, Rectangle, anchorToPivot , Sprite} from 'pixi-svelte';
+	import { Container, Rectangle, Sprite, anchorToPivot } from 'pixi-svelte';
+
 	import { DESKTOP_BASE_SIZE, DESKTOP_BACKGROUND_WIDTH_LIST } from '../game/constants';
-	import { LANDSCAPE_BASE_SIZE, LANDSCAPE_BACKGROUND_WIDTH_LIST } from '../game/constants';
-	import type { LayoutUiProps } from '../game/types';
 	import { getContext } from '../game/context';
+	import type { LayoutUiProps } from '../game/types';
 
 	const props: LayoutUiProps = $props();
+	const baseHeight = DESKTOP_BACKGROUND_WIDTH_LIST.reduce((sum, width) => sum + width, 0);
 	const context = getContext();
+	const layout = () => context.stateLayoutDerived.mainLayoutStandard();
 </script>
-<!-- 
-<Container x={20}>
+
+<!-- <Container x={20}>
 	{@render props.gameName()}
 </Container>
 
@@ -21,114 +23,149 @@
 </Container> -->
 
 <MainContainer standard alignVertical="bottom">
+	<!-- <Rectangle
+		alpha={0.5}
+		backgroundColor={BLACK}
+		anchor={0.5}
+		width={context.stateLayoutDerived.mainLayoutStandard().width*1.2}
+		height={DESKTOP_BASE_SIZE}
+		x={context.stateLayoutDerived.mainLayoutStandard().width * 0.5}
+		y={
+			context.stateLayoutDerived.mainLayoutStandard().height
+			- DESKTOP_BASE_SIZE * 0.4
+			
+		}
+	/> -->
 
 	<Container
 		x={context.stateLayoutDerived.mainLayoutStandard().width * 0.5}
-		y={context.stateLayoutDerived.mainLayoutStandard().height - DESKTOP_BASE_SIZE - 10}
+		y={context.stateLayoutDerived.mainLayoutStandard().height - DESKTOP_BASE_SIZE}
 		pivot={anchorToPivot({
 			anchor: { x: 0.5, y: 0 },
 			sizes: {
-				height: DESKTOP_BASE_SIZE,
+				height: DESKTOP_BASE_SIZE / 6,
 				width: DESKTOP_BACKGROUND_WIDTH_LIST.reduce((sum, width) => sum + width, 0),
 			},
 		})}
 	>
-	<!-- <Rectangle
-	alpha={0.5}
-	backgroundColor={BLACK}
-	width={context.stateLayoutDerived.canvasSizes().width}
-	height={context.stateLayoutDerived.canvasSizes().height}
-	x={context.stateLayoutDerived.canvasSizes().width * 0.5}
-	y={context.stateLayoutDerived.canvasSizes().height * 0.5}
-	anchor={0.5}
-/> -->
-<Sprite	
-		key="desktopBg"
-		anchor={0.5}
-		y={DESKTOP_BASE_SIZE/3} x={DESKTOP_BASE_SIZE * 6.5}
-		scale={{ x:1.8, y: 1.2}}
-	/>
-		<Container y={DESKTOP_BASE_SIZE * 0.42} x={DESKTOP_BASE_SIZE*3.4} scale={0.7}>
-			{@render props.amountBalance({ stacked: true })}
-		</Container>
+		<Container>
+			<Sprite
+				key="desktopBg"
+				anchor={0.5}
+				y={DESKTOP_BASE_SIZE / 3.1}
+				x={DESKTOP_BASE_SIZE * 6.5}
+				scale={{ x: 1.8, y: 1.2 }}
+			/>
+			<Container y={-20}>
+				<Container>
+					<Sprite
+						key="betControl"
+						anchor={0.5}
+						y={70}
+						x={DESKTOP_BASE_SIZE * 3.7}
+						scale={{ x: 1.7, y: 1.5 }}
+					/>
 
-		<Container y={DESKTOP_BASE_SIZE * 0.38} x={DESKTOP_BASE_SIZE*6.4} scale={0.8}>
-			{@render props.amountWin({ stacked: true })}
-		</Container>
+					<Sprite
+						key="betControl"
+						anchor={0.5}
+						y={70}
+						x={DESKTOP_BASE_SIZE * 8.4}
+						scale={{ x: 1.7, y: 1.5 }}
+					/>
 
-		<Container y={DESKTOP_BASE_SIZE * 0.42} x={DESKTOP_BASE_SIZE*9.4} scale={0.7}>
-			{@render props.amountBet({ stacked: true })}
-		</Container>
+					<Sprite
+						key="betControl"
+						anchor={0.5}
+						y={DESKTOP_BASE_SIZE * 0.52}
+						x={DESKTOP_BASE_SIZE * 6.05}
+						scale={{ x: 2.3, y: 1.55 }}
+					/>
+					<Container>
+						<Container y={38} x={DESKTOP_BASE_SIZE * 4.4} scale={0.85}>
+							{@render props.amountBalance({ stacked: true })}
+						</Container>
+						<Container y={38} x={DESKTOP_BASE_SIZE * 9.2} scale={0.85}>
+							{@render props.amountWin({ stacked: true })}
+						</Container>
+						<Container y={38} x={DESKTOP_BASE_SIZE * 6.9} scale={0.85}>
+							{@render props.amountBet({ stacked: true })}
+						</Container>
+					</Container>
+					<Container>
+						<Container
+							y={DESKTOP_BASE_SIZE * 0.45}
+							x={DESKTOP_BASE_SIZE * 10.23}
+							scale={1.18}
+							width={DESKTOP_BASE_SIZE * 1.6}
+						>
+							{@render props.buttonBet({ anchor: 0.5 })}
+						</Container>
 
-		<Container y={DESKTOP_BASE_SIZE * 0.63} x={DESKTOP_BASE_SIZE-150} scale={0.63}>
-			{@render props.buttonMenu({ anchor: 0.5 })}
-		</Container>
+						<Container x={-12}>
+							<Container
+								y={DESKTOP_BASE_SIZE * 0.5}
+								x={DESKTOP_BASE_SIZE * 5.26}
+								scale={{ x: 0.4, y: 0.4 }}
+							>
+								{@render props.buttonDecrease({ anchor: 0.5 })}
+							</Container>
 
-		<Container y={DESKTOP_BASE_SIZE * 1.08} x={DESKTOP_BASE_SIZE*1.1} scale={0.8}>
-			{@render props.buttonBuyBonus({ anchor: 0.5 })}
-		</Container>
-		<Container y={DESKTOP_BASE_SIZE * 0.63} x={DESKTOP_BASE_SIZE*1.5} scale={0.5}>
-			{@render props.buttonSoundSwitch({ anchor: 0.5 })}
-		</Container>
+							<Container y={DESKTOP_BASE_SIZE * 0.5} x={DESKTOP_BASE_SIZE * 7.03} scale={0.4}>
+								{@render props.buttonIncrease({ anchor: 0.5 })}
+							</Container>
+						</Container>
+					</Container>
+				</Container>
 
-		<Container y={DESKTOP_BASE_SIZE * 0.62} x={DESKTOP_BASE_SIZE*13} scale={0.7}>
-			{@render props.buttonAutoSpin({ anchor: 0.5 })}
-		</Container>
+				<Container x={-10}>
+					<Sprite
+						key="vantaLogo"
+						anchor={0.5}
+						y={DESKTOP_BASE_SIZE * 0.4}
+						x={DESKTOP_BASE_SIZE * 1.6}
+						scale={1.5}
+					/>
 
-		<Container y={DESKTOP_BASE_SIZE * 0.4} x={DESKTOP_BASE_SIZE*12.1} scale={1}>
-			{@render props.buttonBet({ anchor: 0.5 })}
-		</Container>
+					<Container y={DESKTOP_BASE_SIZE * 0.7} x={DESKTOP_BASE_SIZE * 2.3} scale={0.3}>
+						{@render props.buttonGameRules({ anchor: 0.5 })}
+					</Container>
+					<Container y={DESKTOP_BASE_SIZE * 0.25} x={DESKTOP_BASE_SIZE * 2.3} scale={0.4}>
+						{@render props.buttonMenu({ anchor: 0.5 })}
+					</Container>
+				</Container>
 
-		<Container y={DESKTOP_BASE_SIZE * 0.62} x={DESKTOP_BASE_SIZE*11.2} scale={0.7}>
-			{@render props.buttonTurbo({ anchor: 0.5 })}
+				<Container y={7} x={40}>
+					<Container
+						y={DESKTOP_BASE_SIZE * 0.65}
+						x={DESKTOP_BASE_SIZE * 11.05}
+						scale={{ x: 0.9, y: 0.3 }}
+					>
+						{@render props.buttonAutoSpin({ anchor: 0.5 })}
+					</Container>
+					<Container
+						y={DESKTOP_BASE_SIZE * 0.29}
+						x={DESKTOP_BASE_SIZE * 11.05}
+						scale={{ x: 0.9, y: 0.3 }}
+					>
+						{@render props.buttonTurbo({ anchor: 0.5 })}
+					</Container>
+				</Container>
+			</Container>
 		</Container>
-
-		<Container y={DESKTOP_BASE_SIZE * 0.65} x={DESKTOP_BASE_SIZE*8.4} scale={0.6}>
-			{@render props.buttonDecrease({ anchor: 0.5 })}
-		</Container>
-
-		<Container y={DESKTOP_BASE_SIZE * 0.65} x={DESKTOP_BASE_SIZE*10.4} scale={0.6}>
-			{@render props.buttonIncrease({ anchor: 0.5 })}
-		</Container>
-		
 	</Container>
 </MainContainer>
 
-{#if stateUi.menuOpen}
-	<Rectangle
-		eventMode="static"
-		cursor="pointer"
-		alpha={0.5}
-		anchor={0.5}
-		backgroundColor={BLACK}
-		width={context.stateLayoutDerived.canvasSizes().width}
-		height={context.stateLayoutDerived.canvasSizes().height}
-		x={context.stateLayoutDerived.canvasSizes().width * 0.5}
-		y={context.stateLayoutDerived.canvasSizes().height * 0.5}
-		onpointerup={() => (stateUi.menuOpen = false)}
-	/>
-
+<!-- {#if stateUi.menuOpen}
 	<MainContainer standard alignVertical="bottom">
 		<Container
-			x={298}
-			y={context.stateLayoutDerived.mainLayoutStandard().height - DESKTOP_BASE_SIZE - 10}
+			x={context.stateLayoutDerived.mainLayoutStandard().width * 0.45}
+			y={context.stateLayoutDerived.mainLayoutStandard().height - DESKTOP_BASE_SIZE-30}
 		>
-			<Container scale={0.8} y={DESKTOP_BASE_SIZE * 0.5 - 150 - 170 * 3}>
-				{@render props.buttonPayTable({ anchor: 0.5 })}
-			</Container>
-
-			<Container scale={0.8} y={DESKTOP_BASE_SIZE * 0.5 - 150 - 170 * 2}>
-				{@render props.buttonGameRules({ anchor: 0.5 })}
-			</Container>
-
-			<Container scale={0.8} y={DESKTOP_BASE_SIZE * 0.5 - 150 - 170 * 1}>
-				{@render props.buttonSettings({ anchor: 0.5 })}
-			</Container>
-
-
-			<Container scale={0.8} y={DESKTOP_BASE_SIZE * 0.5}>
-				{@render props.buttonMenuClose({ anchor: 0.5 })}
+			<Sprite key="menuBackground" anchor={0.5} y={-65} x={-475} scale={{ x: 1.7, y: 1.7 }} />
+			<Container scale={{ x: 1.1, y: 0.2 }} y={-65} x={-450}>
+				{@render props.buttonSoundSwitch({ anchor: 0.5 })}
 			</Container>
 		</Container>
 	</MainContainer>
-{/if}
+{/if} -->
