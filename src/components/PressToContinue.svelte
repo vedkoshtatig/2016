8541f30d62367e743fdccfb774232e27aed725d0
Ved koshta <vkostha@trueigtech.com>
2026-05-08 14:18:34 +0530

    fixed press anywhere to continue

--- src/components/PressToContinue.svelte
+++ src/components/PressToContinue.svelte
@@ -2,27 +2,31 @@
 	import { MainContainer, OnPressFullScreen } from 'components-layout';
 	import { OnHotkey } from 'components-shared';
 	import { stateUrlDerived } from 'state-shared';
-	import { Sprite } from 'pixi-svelte';
+	import { BitmapText} from 'pixi-svelte';
 
 	import { getContext } from '../game/context';
 
 	type Props = {
 		onpress: () => void;
 	};
-
+const fstyle={
+		fontFamily: 'sans',
+		 fontSize: 72, align: 'center' 
+	}
 	const props: Props = $props();
 	const context = getContext();
 </script>
 
 <MainContainer alignVertical="bottom">
-	<Sprite
-		key="pressToContinueText_{stateUrlDerived.lang()}.png"
-		width={800}
-		height={134}
-		anchor={{ x: 0.5, y: 1 }}
+
+	<BitmapText
+							anchor={{ x: 0.5, y: 1 }}
 		x={context.stateLayoutDerived.mainLayout().width * 0.5}
-		y={context.stateLayoutDerived.mainLayout().height}
-	/>
+		y={context.stateLayoutDerived.mainLayout().height/1.05}
+		scale={0.3}
+							text={`PRESS ANYWHERE TO CONTINUE`}
+							style={fstyle}
+						/>
 </MainContainer>
 <OnHotkey hotkey="Space" onpress={() => props.onpress()} />
 <OnPressFullScreen onpress={() => props.onpress()} />
